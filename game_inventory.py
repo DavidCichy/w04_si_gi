26b6cae5c122ca25a7cb4ae317e2416521316107
David Cichy <cichy.dawid@gmail.com>
2019-02-18 20:10:09 +0100

    Stages 1 and 2 done

---
+++ game_inventory.py
@@ -0,0 +1,82 @@
+
+# This is the file where you must work.
+# Write code in the functions (and create new functions) so that they work
+# according to the specification.
+
+
+def display_inventory(inventory):
+    '''Display the inventory like this:
+    rope: 1
+    torch: 6
+
+      '''
+    total_items_count = 0
+    for key, value in inventory.items():
+      print("{} {}".format(value, key))
+      total_items_count += value
+    print("Total number of items: {}".format(total_items_count))
+    
+
+def add_to_inventory(inventory, added_items):
+    '''Add to the inventory dictionary a list of items from added_items.'''
+
+    for item in added_items:
+      if item in inventory:
+        inventory[item] += 1
+      else:
+        inventory[item] = 1
+
+    return inventory
+
+
+def print_table(inventory, order=None):
+    '''
+    Take your inventory and display it in a well-organized table with
+    each column right-justified like this:
+
+    -----------------
+    item name | count
+    -----------------
+         rope |     1
+        torch |     6
+    -----------------
+
+    The 'order' parameter (string) works as follows:
+    - None (by default) means the table is unordered
+    - "count,desc" means the table is ordered by count (of items in the
+      inventory) in descending order
+    - "count,asc" means the table is ordered by count in ascending order
+    '''
+
+    pass
+
+
+def import_inventory(inventory, filename="import_inventory.csv"):
+    '''
+    Import new inventory items from a file.
+
+    The filename comes as an argument, but by default it's
+    "import_inventory.csv". The import automatically merges items by name.
+
+    The file format is plain text with comma separated values (CSV).
+    '''
+
+    pass
+
+
+def export_inventory(inventory, filename="export_inventory.csv"):
+    '''
+    Export the inventory into a .csv file.
+
+    If the filename argument is None, it creates and overwrites a file
+    called "export_inventory.csv".
+
+    The file format is plain text with comma separated values (CSV).
+    '''
+
+    pass
+
+inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = add_to_inventory(inv, dragon_loot)
+display_inventory(inv)
